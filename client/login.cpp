662814afdd8613a9e2d831ae049dbd037514174a
llh <lilinhao0830@gmail.com>
2024-11-23 16:48:31 +0800

    login

---
+++ client/login.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+void welcome();
+int main(){
+    welcome();
+    return 0;
+}
+void welcome(){
+    int flag;
+    std:: cin >> flag;
+    std:: cout << flag <<endl; 
+}
